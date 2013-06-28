1164368eabcd78fd44151b0840993ff329b8a8b0
darkzin <darkzin1021@gmail.com>
2013-06-28 20:04:05 +0900

    add setInterval update_latest_photo, add format.json to index action in photos_controller to deal with update_latest_photo ajax request.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,12 @@ class PhotosController < ApplicationController
   http_basic_authenticate_with name: "skyseed", password:"12345", only: [:create]
 
   def index
-    p request.format
     respond_to do |format|
       format.html {
         @tab_type = "photos"
         if(params[:show_type] == "find")
           @from_datetime = params[:datetimepicker1].to_time
-   	  @to_datetime = params["datetimepicker2"].to_time
+      @to_datetime = params["datetimepicker2"].to_time
           @from_timestamp = @from_datetime.to_time.to_i
           @to_timestamp = @to_datetime.to_time.to_i
 
@@ -18,14 +17,17 @@ class PhotosController < ApplicationController
           @photos = Photo.all.page(params[:page]).per(10)
         end
       }
+
+      format.json {
+        @photos = Photo.where(:created_at.gt => params[:last_update_time])
+        render js: @photos.to_json
+      }
+
       format.js {
-#        p request.headers
-#        @photos = Photo.where(:created_at.gt => params[:last_update_time])
-#        render js: @photos.to_json
-         
+
         if(params[:show_type] == "find")
           @from_datetime = params[:datetimepicker1].to_time
-   	  @to_datetime = params["datetimepicker2"].to_time
+      @to_datetime = params["datetimepicker2"].to_time
           @from_timestamp = @from_datetime.to_time.to_i
           @to_timestamp = @to_datetime.to_time.to_i
 
