82d7414538ac3931c1ada60e0b5777d96e72927a
SungHong-Cho <lunaticbreeze@gmail.com>
2013-07-01 15:40:44 +0900

    edit datetime foramt

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,8 +20,10 @@ class PhotosController < ApplicationController
 
       format.json {
         @photos = Photo.where(:created_at.gt => Time.at(params[:last_update_time].to_i))
-	dateTime = @photos.created_at.to_s
-	@photos.created_at = dateTime[0...dateTime.rindex(" ")]
+        @photos.each {|photo|
+	dateTime = photo.created_at.localtime.to_s
+	photo.created_at = dateTime[0...dateTime.rindex(" ")]
+	}
         render js: @photos.to_json
       }
 
